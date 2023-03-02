e20fd75a0a92c4d0d57e275281f917f09351388a
unknown <marianepagagnotti.sl@gmail.com>
2023-03-02 16:09:40 -0300

    atualizando config e DAO

---
+++ DAO/DAO.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DAO;
+
+use Exception;
+use Exeception;
+use \PDO;
+use PDOException;
+
+
+abstract class DAO extends PDO
+{
+    protected $conexao;
+
+    public function __construct()
+    {
+        try
+        {
+            $options =
+            [
+                PDO:: ATTR_ERRMODE =>PDO::ERRMODE_EXCEPTION,
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+            ];
+
+            $dsn = "mysql:host=" . $_ENV['db']['host'] . ";dbname=" . $_ENV['db']['database'];
+
+            $this->conexao = new PDO($dsn, $_ENV['db']['user'], $_ENV['db']['pass'], $options);
+
+            
+        }
+        catch (PDOException $e){
+
+            throw new Exception("Ocorreu um erro ao tentar conectar com o banco", 0, $e);
+        }
+                
+    }
+}
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('BASEDIR', dirname(__FILE__, 2));
+
+$_ENV['db']['host'] = 'localhost:3307';
+$_ENV['db']['user'] = 'root';
+$_ENV['db']['pass'] = 'etecjau';
+$_ENV['db']['database'] = 'busca_by_cep';
